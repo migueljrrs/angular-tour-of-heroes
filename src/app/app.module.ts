01b9fca17c5927a34e8f860add51cd2fef72aa23
migueljrrs <33840531+migueljrrs@users.noreply.github.com>
2017-11-20 15:10:45 -0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FirebaseConfig } from './../environments/firebase.config';
+import { AngularFireModule } from 'angularfire2/index';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    //MessagesComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(FirebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  providers: [
+    HeroService,
+    MessageService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyAOIsNsZzc8a4db1HmQqILgbDc6LjXm09U",
+  authDomain: "tour-of-heroes-eafb3.firebaseapp.com",
+  databaseURL: "https://tour-of-heroes-eafb3.firebaseio.com",
+  projectId: "tour-of-heroes-eafb3",
+  storageBucket: "tour-of-heroes-eafb3.appspot.com",
+  messagingSenderId: "859495053412"
+};
